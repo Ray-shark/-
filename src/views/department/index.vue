667a4989d26daec95ac1081d4bef3f3579bcb3fa
Ray <2764910376@qq.com>
2023-11-17 17:55:30 +0800

    自定义结构获取作用域插槽数据

--- src/views/department/index.vue
+++ src/views/department/index.vue
@@ -4,23 +4,26 @@
       <!-- 展示组织架构,默认展开子节点 -->
       <el-tree default-expand-all :data="datas" :props="defaultProps">
         <!-- 节点结构 -->
-        <el-row style="width: 100%; height: 40px" type="flex" justify="space-between" align="middle">
-          <el-col>Z-Education</el-col>
-          <el-col :span="4">
-            <span class="tree-manager">管理员</span>
-            <el-dropdown>
-              <!-- 显示区域内容 -->
-              <span class="el-dropdown-link">
-                操作<i class="el-icon-arrow-down el-icon--right" />
+        <!-- v-slot="{ node, data }" 只能作用在template 下面解构出data数据项 -->
+        <template v-slot="{ data }">
+          <el-row style="width: 100%; height: 40px" type="flex" justify="space-between" align="middle">
+            <el-col>{{ data.name }}</el-col>
+            <el-col :span="4">
+              <span class="tree-manager">管理员</span>
+              <el-dropdown>
+                <!-- 显示区域内容 -->
+                <span class="el-dropdown-link">
+                操作<i class="el-icon-arrow-down el-icon--right"/>
               </span>
-              <el-dropdown-menu slot="dropdown">
-                <el-dropdown-item>添加子部门</el-dropdown-item>
-                <el-dropdown-item>编辑部门</el-dropdown-item>
-                <el-dropdown-item>删除部门</el-dropdown-item>
-              </el-dropdown-menu>
-            </el-dropdown>
-          </el-col>
-        </el-row>
+                <el-dropdown-menu slot="dropdown">
+                  <el-dropdown-item>添加子部门</el-dropdown-item>
+                  <el-dropdown-item>编辑部门</el-dropdown-item>
+                  <el-dropdown-item>删除部门</el-dropdown-item>
+                </el-dropdown-menu>
+              </el-dropdown>
+            </el-col>
+          </el-row>
+        </template>
       </el-tree>
     </div>
   </div>
@@ -44,7 +47,7 @@ export default {
       // 与datas中的属性对应
       defaultProps: [{
         label: 'name', // 要显示的字段的名字
-        chidren: 'children' // 读取子节点的字段名
+        children: 'children' // 读取子节点的字段名
       }]
     }
   }
@@ -56,6 +59,7 @@ export default {
   padding: 30px 140px;
   font-size: 14px;
 }
+
 .tree-manager {
   width: 50px;
   display: inline-block;
