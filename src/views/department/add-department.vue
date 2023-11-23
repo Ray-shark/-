87bdcb3fcfc579ccb70f83045ba7930d1bd07f6a
Ray <2764910376@qq.com>
2023-11-23 16:13:23 +0800

    添加部门表单的校验

--- src/views/department/add-department.vue
+++ src/views/department/add-department.vue
@@ -1,21 +1,21 @@
 <template>
   <div>
     <el-dialog title="新增部门" :visible="showDialog" @close="closeDialog">
-      <el-form label-width="100px" class="form">
-        <el-form-item label="部门名称">
-          <el-input style="width: 80%" placeholder="2-10个字符"></el-input>
+      <el-form label-width="100px" class="form" :rules="formRules" :model="dataForm">
+        <el-form-item label="部门名称" prop="depName">
+          <el-input v-model="dataForm.depName" style="width: 90%" placeholder="2-10个字符"/>
         </el-form-item>
-        <el-form-item label="部门编码">
-          <el-input style="width: 80%" placeholder="2-10个字符"></el-input>
+        <el-form-item label="部门编码" prop="depCode">
+          <el-input v-model="dataForm.depCode" style="width: 90%" placeholder="2-10个字符"/>
         </el-form-item>
-        <el-form-item label="部门负责人">
-          <el-select style="width: 80%" placeholder="请选择负责人">
+        <el-form-item label="部门负责人" prop="managerID">
+          <el-select v-model="dataForm.managerID" style="width: 90%" placeholder="请选择负责人">
             <el-option value="a">a</el-option>
             <el-option value="b">b</el-option>
           </el-select>
         </el-form-item>
-        <el-form-item label="部门介绍">
-          <el-input style="width: 80%" :rows="4" type="textarea" placeholder="1-100个字符"></el-input>
+        <el-form-item label="部门介绍" prop="description">
+          <el-input v-model="dataForm.description" style="width: 90%" :rows="4" type="textarea" placeholder="1-100个字符"/>
         </el-form-item>
         <el-form-item>
           <el-button type="primary">确认</el-button>
@@ -27,12 +27,87 @@
 </template>
 
 <script>
+import { getDepartment } from '@/api/department'
+
 export default {
   name: 'AddDepartment',
   props: {
     showDialog: {
       type: Boolean,
       default: false
+    }
+  },
+  data() {
+    return {
+      dataForm: {
+        pid: '', // 父级部门ID
+        depName: '', // 部门名称
+        depCode: '', // 部门编码
+        managerID: '', // 管理员ID
+        description: '' // 部门描述
+      },
+      formRules: {
+        depName: [{
+          required: true,
+          message: '请输入部门名称',
+          trigger: 'blur'
+        }, {
+          min: 2,
+          max: 10,
+          message: '部门名称长度为2-10个字符',
+          trigger: 'blur'
+        }, {
+          trigger: 'blur',
+          // 自定义校验，查看是否和已有名称重复
+          validator: async(rule, value, callback) => {
+            // value就是输入的名称
+            const result = await getDepartment()
+            // result数组中是否存在value值
+            if (result.some(item => item.code === value)) {
+              callback(new Error('部门中已经有该名称了'))
+            } else {
+              callback()
+            }
+          }
+        }],
+        depCode: [{
+          required: true,
+          message: '请输入部门编码',
+          trigger: 'blur'
+        }, {
+          min: 2,
+          max: 10,
+          message: '部门编码长度为2-10个字符',
+          trigger: 'blur'
+        }, {
+          trigger: 'blur',
+          // 自定义校验，查看是否和已有部门重复
+          validator: async(rule, value, callback) => {
+            // value就是输入的编码
+            const result = await getDepartment()
+            // result数组中是否存在value值
+            if (result.some(item => item.code === value)) {
+              callback(new Error('部门中已经有该编码了'))
+            } else {
+              callback()
+            }
+          }
+        }],
+        manager: [{
+          required: true,
+          message: '请选择一个负责人',
+          trigger: 'blur'
+        }],
+        description: [{
+          required: true,
+          message: '请输入部门介绍',
+          trigger: 'blur'
+        }, {
+          max: 100,
+          message: '部门介绍长度为1-100个字符',
+          trigger: 'blur'
+        }]
+      }
     }
   },
   methods: {
@@ -44,7 +119,5 @@ export default {
 </script>
 
 <style scoped>
-.form {
-  text-align: center;
-}
+
 </style>
