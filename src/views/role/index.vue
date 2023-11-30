6927c2514b17887ee749ec0cc1868e3cc82f1009
Ray <2764910376@qq.com>
2023-11-30 17:55:52 +0800

    完成表格列的自定义结构

--- src/views/role/index.vue
+++ src/views/role/index.vue
@@ -9,14 +9,28 @@
       <el-table :data="list">
         <!-- 放置列 -->
         <el-table-column prop="name" align="center" width="200" label="角色"/>
-        <el-table-column prop="state" align="center" width="400" label="启用"/>
-        <el-table-column prop="description" header-align="center" label="描述"/>
-        <el-table-column align="center" label="操作"/>
+        <el-table-column prop="state" align="center" width="200" label="启用">
+          <!-- 自定义的列结构：这一列显示什么内容 -->
+          <!-- 插槽，ElementUI文档中查看传入对象有哪些数据，解构需要的数据 -->
+          <template v-slot="{ row }">
+            <!-- {{row}} -->
+            <span>{{ row.state === 1 ? '已启用' : row.state === 0 ? '未启用' : '无' }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column prop="description" align="center" label="描述"/>
+        <el-table-column align="center" label="操作">
+          <!-- 放置操作按钮 -->
+          <template>
+            <el-button size="mini" type="text">分配权限</el-button>
+            <el-button size="mini" type="text">编辑</el-button>
+            <el-button size="mini" type="text">删除</el-button>
+          </template>
+        </el-table-column>
       </el-table>
       <!-- 放置分页组件 -->
       <el-row type="flex" style="height: 60px" align="middle" justify="end">
         <!-- 放置分页组件 layout属性：自己选择显示什么内容（prev：左箭头，pager：页码，next：右箭头） -->
-        <el-pagination layout="prev, pager, next" />
+        <el-pagination layout="prev, pager, next"/>
       </el-row>
     </div>
   </div>
