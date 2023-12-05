e9fbab1d537a68e8d110dbe8b1c7fa905875f5a3
Ray <2764910376@qq.com>
2023-12-05 16:37:30 +0800

    完成员工表格和分页结构

--- src/views/employee/index.vue
+++ src/views/employee/index.vue
@@ -26,7 +26,29 @@
           <el-button size="mini">excel导出</el-button>
         </el-row>
         <!-- 表格组件 -->
+        <el-table>
+          <el-table-column align="center" label="头像"/>
+          <el-table-column label="姓名"/>
+          <el-table-column label="手机号" sortable/>
+          <el-table-column label="工号" sortable/>
+          <el-table-column label="聘用形式"/>
+          <el-table-column label="部门"/>
+          <el-table-column label="入职时间" sortable/>
+          <el-table-column label="操作" width="280px">
+            <template>
+              <el-button type="text" size="mini">查看</el-button>
+              <el-button type="text" size="mini">角色</el-button>
+              <el-button type="text" size="mini">删除</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
         <!-- 分页 -->
+        <el-row type="flex" style="height: 60px" align="middle" justify="end">
+          <el-pagination
+            layout="total, prev, pager, next"
+            :total="1000"
+          />
+        </el-row>
       </div>
     </div>
   </div>
