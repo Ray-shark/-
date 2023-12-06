e5eaa5bd935bc9a196c0c13b40579be755cee1eb
Ray <2764910376@qq.com>
2023-12-06 14:59:02 +0800

    员工详情的结构和路由配置

--- src/router/modules/employee.js
+++ src/router/modules/employee.js
@@ -10,5 +10,14 @@ export default {
       title: '员工',
       icon: 'people'
     }
+  }, {
+    path: '/employee/detail', // 员工详情的地址
+    component: () => import('@/views/employee/components/detail'),
+    hidden: true, // 表示隐藏在左侧菜单(不显示该二级路由导航)
+    meta: {
+      title: '员工详情'
+    }
   }]
 }
+
+// 只想在左侧导航菜单显示一级菜单：让二级路由只有一个显示在左侧菜单
